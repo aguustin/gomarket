b32ac27d4cab6650fc3dfb05f87a444aed09891f
Agustin Mole <amole@godoycruz.gov.ar>
2025-02-14 12:25:41 -0300

    cambiando proxy

--- client/app/api/blogApi.js
+++ client/app/api/blogApi.js
@@ -1,14 +1,14 @@
 import axios from "axios"
 
-export const createBlogRequest = (blogData) => axios.post('https://100.20.92.101:4000/createBlog', blogData)
+export const createBlogRequest = (blogData) => axios.post('https://https://gomarket-xs2f.onrender.com/createBlog', blogData)
 
-export const updateBlogRequest = (blogData) => axios.post('https://100.20.92.101:4000/updateBlog', blogData)
+export const updateBlogRequest = (blogData) => axios.post('https://https://gomarket-xs2f.onrender.com/updateBlog', blogData)
 
-export const getAllBlogsRequest = () => axios.get('https://100.20.92.101:4000/getAllBlogs')
+export const getAllBlogsRequest = () => axios.get('https://https://gomarket-xs2f.onrender.com/getAllBlogs')
 
-export const getBlogByIdRequest = (blogId) => axios.get(`https://100.20.92.101:4000/Blogs/${blogId}`)
+export const getBlogByIdRequest = (blogId) => axios.get(`https://https://gomarket-xs2f.onrender.com/Blogs/${blogId}`)
 
-export const deleteBlogRequest = (blogId) => axios.put(`https://100.20.92.101:4000/deleteBlog/${blogId}`)
+export const deleteBlogRequest = (blogId) => axios.put(`https://https://gomarket-xs2f.onrender.com/deleteBlog/${blogId}`)
 
 /*export const createBlogRequest = (blogData) => axios.post('http://127.0.0.1:4000/createBlog', blogData)
 
