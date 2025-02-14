3692b70f8ae20f282f88d236f500e70b54df6da5
Agustin Mole <amole@godoycruz.gov.ar>
2025-02-14 10:33:25 -0300

    events borrado

--- client/app/api/blogApi.js
+++ client/app/api/blogApi.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
-export const createBlogRequest = (blogData) => axios.post('http://200.32.101.183:4000/createBlog', blogData)
+export const createBlogRequest = (blogData) => axios.post('http://100.20.92.101:4000/createBlog', blogData)
 
-export const updateBlogRequest = (blogData) => axios.post('http://200.32.101.183:4000/updateBlog', blogData)
+export const updateBlogRequest = (blogData) => axios.post('http://100.20.92.101:4000/updateBlog', blogData)
 
-export const getAllBlogsRequest = () => axios.get('http://200.32.101.183:4000/getAllBlogs')
+export const getAllBlogsRequest = () => axios.get('http://100.20.92.101:4000/getAllBlogs')
 
-export const getBlogByIdRequest = (blogId) => axios.get(`http://200.32.101.183:4000/Blogs/${blogId}`)
+export const getBlogByIdRequest = (blogId) => axios.get(`http://100.20.92.101:4000/Blogs/${blogId}`)
 
-export const deleteBlogRequest = (blogId) => axios.put(`http://200.32.101.183:4000/deleteBlog/${blogId}`)
+export const deleteBlogRequest = (blogId) => axios.put(`http://100.20.92.101:4000/deleteBlog/${blogId}`)
--- client/app/api/userApi.js
+++ client/app/api/userApi.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-export const signInRequest = (userData) => axios.post('http://200.32.101.183:4000/signIn', userData)
+export const signInRequest = (userData) => axios.post('http://100.20.92.101:4000/signIn', userData)
 
-export const loginRequest = (loginData) => axios.post('http://200.32.101.183:4000/login', loginData)
+export const loginRequest = (loginData) => axios.post('http://100.20.92.101:4000/login', loginData)
 
-export const getUsersRequest = () => axios.get('http://200.32.101.183:4000/getUsers')
+export const getUsersRequest = () => axios.get('http://100.20.92.101:4000/getUsers')
