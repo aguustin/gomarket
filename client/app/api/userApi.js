882ee661561f516ff6a2f7ece94e1949538412ee
Agustin Mole <amole@godoycruz.gov.ar>
2025-02-14 13:42:49 -0300

    cambiando proxy

--- client/app/api/userApi.js
+++ client/app/api/userApi.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 
-export const signInRequest = (userData) => axios.post('https://gomarket-xs2f.onrender.com/signIn', userData)
+export const signInRequest = (userData) => axios.post('https://100.20.92.101:4000/signIn', userData)
 
-export const loginRequest = (loginData) => axios.post('https://gomarket-xs2f.onrender.com/login', loginData)
+export const loginRequest = (loginData) => axios.post('https://100.20.92.101:4000/login', loginData)
 
-export const getUsersRequest = () => axios.get('https://gomarket-xs2f.onrender.com/getUsers')
+export const getUsersRequest = () => axios.get('https://100.20.92.101:4000/getUsers')
 
 
 /*export const signInRequest = (userData) => axios.post('http://127.0.0.1:4000/signIn', userData)
