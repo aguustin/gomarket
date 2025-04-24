a3bf71554619a1b54f85e96f5326d98acf77357a
Agustin Mole <amole@godoycruz.gov.ar>
2025-04-24 08:50:11 -0300

    haciendo backend

--- client/app/api/blogApi.js
+++ client/app/api/blogApi.js
@@ -1,14 +1,14 @@
 import axios from "axios"
 
-export const createBlogRequest = (blogData) => axios.post('https://gomarket-1.onrender.com/createBlog', blogData)
+export const createBlogRequest = (blogData) => axios.post('https://gomarket-1-backend.onrender.com/createBlog', blogData)
 
-export const updateBlogRequest = (blogData) => axios.post('https://gomarket-1.onrender.com/updateBlog', blogData)
+export const updateBlogRequest = (blogData) => axios.post('https://gomarket-1-backend.onrender.com/updateBlog', blogData)
 
-export const getAllBlogsRequest = () => axios.get('https://gomarket-1.onrender.com/getAllBlogs')
+export const getAllBlogsRequest = () => axios.get('https://gomarket-1-backend.onrender.com/getAllBlogs')
 
-export const getBlogByIdRequest = (blogId) => axios.get(`https://gomarket-1.onrender.com/Blogs/${blogId}`)
+export const getBlogByIdRequest = (blogId) => axios.get(`https://gomarket-1-backend.onrender.com/Blogs/${blogId}`)
 
-export const deleteBlogRequest = (blogId) => axios.put(`https://gomarket-1.onrender.com/deleteBlog/${blogId}`)
+export const deleteBlogRequest = (blogId) => axios.put(`https://gomarket-1-backend.onrender.com/deleteBlog/${blogId}`)
 
 /*export const createBlogRequest = (blogData) => axios.post('http://127.0.0.1:4000/createBlog', blogData)
 
--- client/app/api/userApi.js
+++ client/app/api/userApi.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 
-export const signInRequest = (userData) => axios.post('https://gomarket-1.onrender.com/signIn', userData)
+export const signInRequest = (userData) => axios.post('https://gomarket-1-backend.onrender.com/signIn', userData)
 
-export const loginRequest = (loginData) => axios.post('https://gomarket-1.onrender.com/login', loginData)
+export const loginRequest = (loginData) => axios.post('https://gomarket-1-backend.onrender.com/login', loginData)
 
-export const getUsersRequest = () => axios.get('https://gomarket-1.onrender.com/getUsers')
+export const getUsersRequest = () => axios.get('https://gomarket-1-backend.onrender.com/getUsers')
 
 
 /*export const signInRequest = (userData) => axios.post('http://127.0.0.1:4000/signIn', userData)
