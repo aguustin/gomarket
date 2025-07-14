8d2dff9a653a2f7577e41bdb6576fd5a8903c34e
Agustin Mole <amole@godoycruz.gov.ar>
2025-07-14 12:08:10 -0300

    iniciando el pago en gomarket

--- client/app/ticket/validate/page.jsx
+++ client/app/ticket/validate/page.jsx
@@ -2,6 +2,8 @@
 import Show from '../../Show/page';
 import { useSearchParams } from 'next/navigation';
 
+export const dynamic = "force-dynamic";
+
 export default function ValidatePage() {
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
