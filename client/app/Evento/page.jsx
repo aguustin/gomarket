29c271f12751e96c0bbfbea640bdfc252e5be197
aguustin <94425896+aguustin@users.noreply.github.com>
2025-08-02 14:40:47 -0300

    Update page.jsx

--- client/app/Evento/page.jsx
+++ client/app/Evento/page.jsx
@@ -124,6 +124,7 @@ const Evento = () => {
                     </div>
                    } 
                 <p className="text-2xl mt-6">Total:${total}</p>
+                <p className="text-xl mt-6">Se cargara un 10% al monto total por el servicio</p>
                 {message && <p className="text-xl text-red-600 mt-4">Los emails no coinciden</p>}
                 <button className="bg-violet-900 p-4 mt-6 w-[280px] rounded-lg text-2xl cursor-pointer" type="submit">Comprar</button>
             </form>
