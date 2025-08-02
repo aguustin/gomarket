cbdbf0457552b6bdd395aa3fc90b2e79e395db48
aguustin <94425896+aguustin@users.noreply.github.com>
2025-08-02 14:56:39 -0300

    Update eventModel.js

--- server/models/eventModel.js
+++ server/models/eventModel.js
@@ -4,7 +4,8 @@ const eventModel = mongoose.Schema({
     token:{type:String},
     nombreCompleto:{type: String},
     dni:{type: Number},
-    used:{type:Boolean}
+    used:{type:Boolean},
+    mail: {type: String}
 })
 
 export default mongoose.model('event', eventModel)
